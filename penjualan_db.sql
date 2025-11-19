8a68284305080a52dc76f579a609120397134c74
Muhammad Rama Raditya <154063534+Rama-Raditya@users.noreply.github.com>
2025-11-19 13:19:36 +0700

    db

---
+++ penjualan_db.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Nov 2025 pada 07.17
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kategori` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
+(1, 'Elektronik'),
+(2, 'Aksesoris'),
+(3, 'Fashion');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` text NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `price`, `description`, `category`, `image`) VALUES
+(1, 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops', 150000.00, 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday', 'Fashion', 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png'),
+(2, 'Mens Casual Premium Slim Fit T-Shirts', 300000.00, 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight &amp;amp;amp;amp; soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.', 'Fashion', 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png'),
+(3, 'Mens Cotton Jacket', 900000.00, 'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.', 'Fashion', 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'),
+(4, 'Mens Casual Slim Fit mamat', 149999.99, 'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.', 'Fashion', 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png'),
+(5, 'John Hardy Women\'s Legends Naga Gold & Silver Dragon Station Chain Bracelet', 695.00, 'From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean\'s pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.', 'Aksesoris', 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png'),
+(6, 'Solid Gold Petite Micropave', 168.00, 'Satisfaction Guaranteed. Return or exchange any order within 30 days. Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.', 'Aksesoris', 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png'),
+(7, 'White Gold Plated Princess', 9.99, 'Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine\'s Day...', 'Aksesoris', 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png'),
+(8, 'Pierced Owl Rose Gold Plated Stainless Steel Double', 10.99, 'Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel', 'Aksesoris', 'https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png'),
+(9, 'WD 2TB Elements Portable External Hard Drive - USB 3.0', 64.00, 'USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system', 'Elektronik', 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_t.png'),
+(10, 'SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s', 109.00, 'Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)', 'Elektronik', 'https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_t.png'),
+(11, 'Plus Men&amp;#039;s Light Blue Regular Fit Washed Jeans Stretchable', 200000.00, 'Make heads turn in this latest design of Urbano Plus Men&amp;#039;s Light Blue Regular Fit Shaded Washed Stretch Jeans, which brings the latest trend for the fashion conscious. High on style and quality, these uber cool &amp;amp; cult whisker-washed stretchable jeans are as versatile as they are comfortable - a must have in your wardrobe for all seasons. Perfect for casual &amp;amp; party wear, pair these in-vogue mid-rise jeans with any dark color Urbano Plus shirt or t-shirt, and never go out of style! So go ahead, stand apart from the crowd and create your own trends. And follow no one else but yourself. Urbano Plus - your go-to stylish and comfortable Plus size brand from Urbano Fashion.', 'Fashion', 'https://www.urbanofashion.com/cdn/shop/files/pluscfjeanp-001-lblue-1.jpg'),
+(12, 'SETELAN BAJU WAROK PONOROGO / BAJU REOG PONOROGO', 200000.00, 'Satu setel terdiri dari:\nPenadon\nKaos Reog Lurik\nCelana Kombor List Merah\nTali Kolor\n\nBahan berkualitas halus jahitan rapi\n\nNyaman dipakai Sejuk\nAsli Dikirim Dari Ponorogo\nUkuran S-M-L-XL-XXL', 'Fashion', 'https://img.lazcdn.com/g/p/55bed1c38bd3614663382961ee90777e.jpg_960x960q80.jpg_.webp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
